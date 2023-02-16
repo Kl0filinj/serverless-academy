58bb6e7a61dbbf1069f842151eb1d58ed80d7449
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-02-16 19:56:12 +0100

    done

--- 06_cli_instagram_giveaway/app.js
+++ 06_cli_instagram_giveaway/app.js
@@ -1,18 +1,45 @@
 const { readFile, writeFile } = require("node:fs/promises");
 const fs = require("fs");
-// Function to get current filenames
-// in directory
+
 const dataPath = __dirname + "/data";
 
-async function getUniqueCount() {
+// high order fnc to get time record
+async function getTimeRecord(fnc) {
+  const start = new Date().getTime();
+  await fnc();
+  const end = new Date().getTime();
+  console.log(`${end - start}ms`);
+}
+
+async function uniqueValues() {
   const filenames = fs.readdirSync(dataPath);
   const uniqueUsers = [];
   for (let i = 0; i < filenames.length; i += 1) {
-    console.log(i);
     const users = await readFile(dataPath + `/${filenames[i]}`, "utf8");
     uniqueUsers.push(...users.split("\n"));
   }
   const set = new Set(uniqueUsers);
   console.log(set.size);
 }
-getUniqueCount();
+
+async function existInAllFiles() {
+  const filenames = fs.readdirSync(dataPath);
+  const uniqueUsers = [];
+  for (let i = 0; i < filenames.length; i += 1) {
+    const users = await readFile(dataPath + `/${filenames[i]}`, "utf8");
+    uniqueUsers.push(...users.split("\n"));
+  }
+  console.log(uniqueUsers.length);
+}
+
+async function existInAtleastTen() {
+  const filenames = fs.readdirSync(dataPath);
+  const uniqueUsers = [];
+  for (let i = 0; i < 10; i += 1) {
+    const users = await readFile(dataPath + `/${filenames[i]}`, "utf8");
+    uniqueUsers.push(...users.split("\n"));
+  }
+  console.log(uniqueUsers.length);
+}
+
+getTimeRecord(existInAtleastTen);
