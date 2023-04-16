026721ff9033b81b21bdedd77cebbb8817acaa4b
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-04-16 14:02:56 +0200

    Home page is adapted for tablets

--- TINVIO_LANDING_PAGES/src/components/Header/Header.styled.ts
+++ TINVIO_LANDING_PAGES/src/components/Header/Header.styled.ts
@@ -1,7 +1,9 @@
 import { theme } from "@/theme";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { ContentContainer } from "../shared/styles/shearedStyles";
+import { ContentContainer, SvgContainer } from "../shared/styles/shearedStyles";
+import medDotsGroupIcon from "@/assets/shared/dots/med_17_dots_group.svg";
+import bigDotsGroupIcon from "@/assets/shared/dots/big_25_dots_group.svg";
 
 interface HeaderNavContainerProps {
   isMenuOpen?: boolean;
@@ -48,17 +50,54 @@ export const HeaderNavList = styled.ul`
   padding: 0px;
 `;
 
+export const HeaderAddictionInfo = styled.div`
+  display: none;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 32px;
+
+  @media screen and (min-width: ${theme.breakPoints.sm}) {
+    display: flex;
+  }
+`;
+
 export const HeaderNavContainer = styled.div<HeaderNavContainerProps>`
   display: ${(p) => (p.isMenuOpen ? "block" : "none")};
   background-color: ${theme.colors.white};
   margin-top: 92px;
+  overflow-y: scroll;
+  padding-bottom: 92px;
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
   z-index: 999;
-  /* transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1); */
+
+  @media screen and (min-width: ${theme.breakPoints.sm}) {
+    display: ${(p) => (p.isMenuOpen ? "flex" : "none")};
+    justify-content: space-evenly;
+    flex-direction: column;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    background-image: url(${medDotsGroupIcon});
+    bottom: 98px;
+    right: 14px;
+    width: 153px;
+    height: 187px;
+
+    @media screen and (min-width: ${theme.breakPoints.sm}) {
+      background-image: url(${bigDotsGroupIcon});
+      width: 359px;
+      height: 273px;
+      bottom: 276px;
+      right: -137px;
+    }
+  }
 `;
 
 export const HeaderNavLinks = styled(NavLink)`
@@ -78,4 +117,9 @@ export const HeaderContentContainer = styled(ContentContainer)`
   @media screen and (min-width: ${theme.breakPoints.sm}) {
     max-width: ${theme.breakPoints.sm};
   }
+`;
+
+export const BurgerMenuDotsIcon = styled(SvgContainer)`
+  bottom: 98px;
+  right: -60px;
 `;
--- TINVIO_LANDING_PAGES/src/components/Header/Header.tsx
+++ TINVIO_LANDING_PAGES/src/components/Header/Header.tsx
@@ -8,13 +8,24 @@ import {
   HeaderNavContainer,
   HeaderNavLinks,
   HeaderContentContainer,
+  HeaderAddictionInfo,
+  BurgerMenuDotsIcon,
 } from "./Header.styled";
-import { SvgContainer } from "../shared/styles/shearedStyles";
+import { AccentButton, SvgContainer } from "../shared/styles/shearedStyles";
 
 import logo from "@/assets/shared/header/tinvio_logo.svg";
 import burgerMenuIcon from "@/assets/shared/header/burger_menu_btn.svg";
 import crossBurgerMenuIcon from "@/assets/shared/header/cross_burger_icon.svg";
-import medDotsGroupIcon from "@/assets/shared/dots/med_17_dots_group.svg";
+
+import {
+  FooterSocContainer,
+  FooterSocInnerContainer,
+} from "../Footer/Footer.styled";
+
+import linkedinIcon from "@/assets/shared/footer/linkedin_footer_icon.svg";
+import instagramIcon from "@/assets/shared/footer/instagram_footer_icon.svg";
+import googlePlayIcon from "@/assets/shared/footer/google_play_footer_icon.svg";
+import appStoreIcon from "@/assets/shared/footer/app_store_footer_icon.svg";
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -38,11 +49,11 @@ const Header: React.FC = () => {
             )}
           </BurgerMenuBtn>
           <HeaderNavContainer isMenuOpen={isMenuOpen}>
-            {isMenuOpen && (
-              <SvgContainer styles={{ bottom: "98px", right: "-60px" }}>
+            {/* {isMenuOpen && (
+              <BurgerMenuDotsIcon>
                 <img src={medDotsGroupIcon} alt="Dots Icon" />
-              </SvgContainer>
-            )}
+              </BurgerMenuDotsIcon>
+            )} */}
             <HeaderNav>
               <HeaderNavList>
                 <li>
@@ -56,6 +67,29 @@ const Header: React.FC = () => {
                 </li>
               </HeaderNavList>
             </HeaderNav>
+            <HeaderAddictionInfo>
+              <div>
+                <AccentButton>Get Started</AccentButton>
+              </div>
+              <FooterSocContainer>
+                <FooterSocInnerContainer>
+                  <div>
+                    <img src={linkedinIcon} alt="Linkedin Icon" />
+                  </div>
+                  <div>
+                    <img src={instagramIcon} alt="Instagram Icon" />
+                  </div>
+                </FooterSocInnerContainer>
+                <FooterSocInnerContainer>
+                  <div>
+                    <img src={googlePlayIcon} alt="Google Play Icon" />
+                  </div>
+                  <div>
+                    <img src={appStoreIcon} alt="App Store Icon" />
+                  </div>
+                </FooterSocInnerContainer>
+              </FooterSocContainer>
+            </HeaderAddictionInfo>
           </HeaderNavContainer>
         </HeaderInnerContainer>
       </HeaderContentContainer>
